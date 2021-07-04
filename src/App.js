69be1c18d56cfe33eb4ecf0895cf37ce11f11504
Bo-Shan Xiang <bo-shanxiang@Bo-Shans-MacBook-Pro.local>
2021-07-04 17:30:43 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseURL = 'http://localhost:8000/'
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      legal_entities: [],
+      natural_persons: [],
+      equity_classes: [],
+      equity_tokens: [],
+      employment_relations: [],
+      officer_relations: [],
+      director_relations: []
+
+      
+    }
+
+    this.getLegalEntities = this.getLegalEntities.bind(this)
+    this.getNaturalPersons = this.getNaturalPersons.bind(this)
+  }
+
+  getLegalEntities() {
+    fetch(baseURL + 'legalentities')
+      .then(data => {return data.json()}, err => console.log(err))
+      .then(parsedData => {this.setState({legal_entities: parsedData})}, err => console.log(err))
+      .catch((e) => console.log(e.message))
+  }
+
+  getNaturalPersons() {
+    fetch(baseURL + 'naturalpersons')
+      .then(data => {return data.json()}, err => console.log(err))
+      .then(parsedData => {this.setState({natural_persons: parsedData})}, err => console.log(err))
+      .catch((e) => console.log(e.message))
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            1
+          </li>
+          <li>
+            2
+          </li>
+          <li>
+            3
+          </li>
+        </ul>
+
+      </div>
+    )
+  }
 }
 
 export default App;
